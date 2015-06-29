bfa22c6df5f8cb030a5f72df63f07cf3740cca30
Salomao Santos <salomao@hotmail.com>
2015-06-29 17:01:36 -0300

    Exemplo de aplicação com melhores práticas no ZendFramework2

---
+++ module/Blog/config/module.config.php
@@ -0,0 +1,95 @@
+<?php
+
+return array(
+    // Service Manager Registry
+    'service_manager' => array(
+        'factories' => array(
+           'Blog\Mapper\PostMapperInterface'   => 'Blog\Factory\ZendDbSqlMapperFactory',
+           'Blog\Service\PostServiceInterface' => 'Blog\Factory\PostServiceFactory',
+           'Zend\Db\Adapter\Adapter'           => 'Zend\Db\Adapter\AdapterServiceFactory'
+        )
+    ),
+
+    // Template Views
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+    // Factories OR Invokables
+    'controllers' => array(
+        'factories' => array(
+            'Blog\Controller\List'   => 'Blog\Factory\ListControllerFactory',
+            'Blog\Controller\Write'  => 'Blog\Factory\WriteControllerFactory',
+            'Blog\Controller\Delete' => 'Blog\Factory\DeleteControllerFactory'
+        )
+    ),
+
+    // This lines opens the configuration for the RouteManager
+    'router' => array(
+        'routes' => array(
+            'blog' => array(
+                'type' => 'literal',
+                'options' => array(
+                    'route'    => '/blog',
+                    'defaults' => array(
+                        'controller' => 'Blog\Controller\List',
+                        'action'     => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes'  => array(
+                    'detail' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => '/:id',
+                            'defaults' => array(
+                                'action' => 'detail'
+                            ),
+                            'constraints' => array(
+                                'id' => '[1-9]\d*'
+                            )
+                        )
+                    ),
+                    'add' => array(
+                        'type' => 'literal',
+                        'options' => array(
+                            'route'    => '/add',
+                            'defaults' => array(
+                                'controller' => 'Blog\Controller\Write',
+                                'action'     => 'add'
+                            )
+                        )
+                    ),
+                    'edit' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => '/edit/:id',
+                            'defaults' => array(
+                                'controller' => 'Blog\Controller\Write',
+                                'action'     => 'edit'
+                            ),
+                            'constraints' => array(
+                                'id' => '\d+'
+                            )
+                        )
+                    ),
+                    'delete' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route'    => '/delete/:id',
+                            'defaults' => array(
+                                'controller' => 'Blog\Controller\Delete',
+                                'action'     => 'delete'
+                            ),
+                            'constraints' => array(
+                                'id' => '\d+'
+                            )
+                        )
+                    ),                    
+                )
+            )
+        )
+    )
+);
---
+++ module/Vip/src/Vip/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salo
+ * Date: 25/03/15
+ * Time: 15:14
+ */
+
+namespace Vip\Controller;
+
+
+class IndexController {
+
+} 
